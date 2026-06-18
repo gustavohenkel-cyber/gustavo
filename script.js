405041b555ddb5447f11f129cddf7b77e7721e12
gustavohenkel-cyber <gustavo_henkel@estudante.sesisenai.org.br>
2026-06-18 17:27:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+function guilhermeportoes(){
+
+    let clts = Number(prompt("quantos clts?"))
+    let estagiarios = Number(prompt("quantos estagiarios")) 
+    let pj = Number(prompt("quantos pj"))
+    let funcionarios = estagiarios = pj
+    alert("a quantidade de funcionarios e" + funcionarios)
+
+}
+
+function banheiro(){
+    alert("ja fez o exercicio 2?")
+
+}
+
+function pePequeno(){
+    letqtdPares = Number(prompt("quantos pares?"))
+    let precoPar = Number(prompt("preco de cada"))
+
+    let total = qtdPares * precoPar
+    alert("valor total R$" + total)
+
+}
+
+
+function peba(){
+
+
+    
+    // ler o numero de vitorias
+    let vitorias = Number(prompt("quantas vitorias?"))
+    //ler o numero de empates
+    let empates = Number(prompt("quantos empates"))
+    //calcular pontos
+    let pontos = vitorias*3 + empates
+    //mostrar resultados
+    alert("o time teu tem " + pontos)
+}
